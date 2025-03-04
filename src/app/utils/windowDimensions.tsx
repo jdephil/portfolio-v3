29ff0e8d5e990b3db39b31c08977d588e0ad1f41
Jennifer De Phillips <jennifer@thinknimble.com>
2025-03-04 16:35:42 -0500

    window not null

--- src/app/utils/windowDimensions.tsx
+++ src/app/utils/windowDimensions.tsx
@@ -1,26 +1,31 @@
-import { useEffect, useState } from "react"
-
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return {
-    width,
-    height,
-  }
-}
+import { useState, useEffect } from "react"
 
 export default function useWindowDimensions() {
+  const hasWindow = typeof window !== "undefined"
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : 0
+    const height = hasWindow ? window.innerHeight : 0
+    return {
+      width,
+      height,
+    }
+  }
+
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions(),
   )
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions())
-    }
+    if (hasWindow) {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions())
+      }
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
+      window.addEventListener("resize", handleResize)
+      return () => window.removeEventListener("resize", handleResize)
+    }
+  }, [hasWindow])
 
   return windowDimensions
 }
